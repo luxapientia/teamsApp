471443c074a14c487a12efd67fa9fb9594a8c1cf
luxapientia <trongtruong.pham192@gmail.com>
2025-03-28 00:15:17 +0700

    Add the collapse feature in the annualtarget table.

---
+++ src/pages/scorecards/components/PerspectiveTab.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  Box,
+  Typography,
+} from '@mui/material';
+
+const PerspectiveTab: React.FC = () => {
+  return (
+    <Box p={2}>
+      <Typography>Financial Perspective</Typography>
+      {/* Add your perspectives content here */}
+    </Box>
+  );
+};
+
+export default PerspectiveTab;
