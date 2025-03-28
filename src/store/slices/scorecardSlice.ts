abc1b712e37500117718484b12ad30d7c00354ae
luxapientia <trongtruong.pham192@gmail.com>
2025-03-29 01:29:38 +0700

    fix initial state in redux

--- src/store/slices/scorecardSlice.ts
+++ src/store/slices/scorecardSlice.ts
@@ -21,38 +21,38 @@ const initialState: ScorecardState = {
         ratingScores: [],
         assesmentPeriod: {
           Q1: {
-            startDate: '',
-            endDate: '',
+            startDate: '2020-01-01',
+            endDate: '2020-12-31',
           },
           Q2: {
-            startDate: '',
-            endDate: '',
+            startDate: '2020-01-01',
+            endDate: '2020-12-31',
           },
           Q3: {
-            startDate: '',
-            endDate: '',
+            startDate: '2020-01-01',
+            endDate: '2020-12-31',
           },
           Q4: {
-            startDate: '',
-            endDate: '',
+            startDate: '2020-01-01',
+            endDate: '2020-12-31',
           },
         },
         contractingPeriod: {
           Q1: {
-            startDate: '',
-            endDate: '',
+            startDate: '2020-01-01',
+            endDate: '2020-12-31',
           },
           Q2: {
-            startDate: '',
-            endDate: '',
+            startDate: '2020-01-01',
+            endDate: '2020-12-31',
           },
           Q3: {
-            startDate: '',
-            endDate: '',
+            startDate: '2020-01-01',
+            endDate: '2020-12-31',
           },
           Q4: {
-            startDate: '',
-            endDate: '',
+            startDate: '2020-01-01',
+            endDate: '2020-12-31',
           },
         },
       },
