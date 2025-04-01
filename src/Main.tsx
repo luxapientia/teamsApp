bc9528c87d3d9a74cbf0b73a30f6eef82275e4db
Truong Pham <skyeline.dreamer000@gmail.com>
2025-04-01 03:36:37 +0300

    Quarterly Notification

--- src/Main.tsx
+++ src/Main.tsx
@@ -4,11 +4,13 @@ import './styles/globals.css';
 import AnnualCorporateScorecard from './pages/scorecards';
 import Teams from './pages/teams';
 import { ManagePage } from './pages/manage';
-import { BoardSplitRegular, GridRegular, PeopleTeamRegular, Organization20Regular } from '@fluentui/react-icons';
+import { BoardSplitRegular, GridRegular, PeopleTeamRegular, Organization20Regular, Alert24Regular } from '@fluentui/react-icons';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { useAuth } from './contexts/AuthContext';
 import OrganizationPerformance from './pages/organization_performance';
+import NotificationPage from './pages/notification';
+
 const iconSize = 24;
 
 function Main() {
@@ -47,6 +49,12 @@ function Main() {
           title='Organization Performance'
           icon={<Organization20Regular fontSize={iconSize} />}
           tabs={['Performance Evaluations', 'Organization Performance']}
+          selectedTab={selectedTab}
+        />
+        <NotificationPage 
+          title='Notifications'
+          icon={<Alert24Regular fontSize={iconSize} />}
+          tabs={['Quarterly Targets', 'Performance Assessments']}
           selectedTab={selectedTab}
         />
       </Layout>
