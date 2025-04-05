a560d6c7a872bb067b195e55daef96bba5296ad4
luxapientia <trongtruong.pham192@gmail.com>
2025-04-05 16:17:29 +0700

    fix the import url

--- src/pages/my_performance_assessment/index.tsx
+++ src/pages/my_performance_assessment/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { Box } from '@mui/material';
 import { PageProps } from '../../types';
-import PerformanceAssessment from './performance_assessment';
+import PerformanceAssessment from './my_assessment';
 import { useAppDispatch } from '../../hooks/useAppDispatch';
 import { fetchAnnualTargets } from '../../store/slices/scorecardSlice';
 const MyPerformanceAssessment: React.FC<PageProps> = ({ title, icon, tabs, selectedTab }) => {
