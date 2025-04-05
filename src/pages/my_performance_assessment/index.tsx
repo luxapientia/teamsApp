ba234f6bdf7e3325543a65dc8835630a896dbcbd
luxapientia <trongtruong.pham192@gmail.com>
2025-04-05 17:44:44 +0700

    Add Team Performances tab

--- src/pages/my_performance_assessment/index.tsx
+++ src/pages/my_performance_assessment/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Box } from '@mui/material';
 import { PageProps } from '../../types';
 import PerformanceAssessment from './my_assessment';
+import TeamPerformance from './team_performance';
 import { useAppDispatch } from '../../hooks/useAppDispatch';
 import { fetchAnnualTargets } from '../../store/slices/scorecardSlice';
 const MyPerformanceAssessment: React.FC<PageProps> = ({ title, icon, tabs, selectedTab }) => {
@@ -13,7 +14,12 @@ const MyPerformanceAssessment: React.FC<PageProps> = ({ title, icon, tabs, selec
 
   return (
     <Box>
-      <PerformanceAssessment />
+      {selectedTab === 'My Assessments' && (
+        <PerformanceAssessment />
+      )}
+      {selectedTab === 'Team Performances' && (
+        <TeamPerformance />
+      )}
     </Box>
   );
 };
