309a4ae313cd1dd7e44cf480b212302e36bab8fc
luxapientia <trongtruong.pham192@gmail.com>
2025-04-06 07:19:24 +0700

    Add team agreement, assessment completions tab

--- src/pages/reports/index.tsx
+++ src/pages/reports/index.tsx
@@ -6,6 +6,9 @@ import { fetchAnnualTargets } from '../../store/slices/scorecardSlice';
 import TeamPerformanceAgreements from './team_performance_agreements';
 import TeamPerformanceAssessment from './team_performance_assessment';
 import TeamPerformances from './team_performances';
+import TeamPerformanceAgreementsCompletions from './team_performance_agreements_completions';
+import TeamPerformanceAssessmentsCompletions from './team_performance_assessments_completions';
+
 const MyPerformanceAssessment: React.FC<PageProps> = ({ title, icon, tabs, selectedTab }) => {
   const dispatch = useAppDispatch();
 
@@ -21,6 +24,10 @@ const MyPerformanceAssessment: React.FC<PageProps> = ({ title, icon, tabs, selec
         return <TeamPerformanceAssessment />;
       case 'Teams Performances':
         return <TeamPerformances />;
+      case 'Teams Performance Agreements Completions':
+        return <TeamPerformanceAgreementsCompletions />;
+      case 'Teams Performance Assessments Completions':
+        return <TeamPerformanceAssessmentsCompletions />;
       default:
         return null;
     }
