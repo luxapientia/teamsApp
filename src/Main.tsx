cf79eea71ea63b60d30bd61ca3d251e45b3badd6
Javid Jarafrov <javid.jafarov.ag@gmail.com>
2025-04-06 15:44:05 +0300

    change list icons in sidebar

--- src/Main.tsx
+++ src/Main.tsx
@@ -4,7 +4,7 @@ import './styles/globals.css';
 import AnnualCorporateScorecard from './pages/scorecards';
 import Teams from './pages/teams';
 import { ManagePage } from './pages/manage';
-import { BoardSplitRegular, GridRegular, PeopleTeamRegular, Organization20Regular, Alert24Regular } from '@fluentui/react-icons';
+import { GridRegular, Alert24Regular, DocumentText24Regular, ClipboardCheckmark24Regular, DataTrending24Regular, Handshake24Regular, PeopleTeam24Regular, Globe24Regular } from '@fluentui/react-icons';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { useAuth } from './contexts/AuthContext';
@@ -37,37 +37,37 @@ function Main() {
         )}
         <Reports
           title='Reports'
-          icon={<Organization20Regular fontSize={iconSize} />}
+          icon={<DocumentText24Regular fontSize={iconSize} />}
           tabs={['Teams Performances', 'Teams Performance Assessments Completions', 'Teams Performance Agreements Completions', 'Teams Performance Assessments', 'Teams Performance Agreements']}
           selectedTab={selectedTab}
         />
         <MyPerformanceAssessment
           title='My Performance Assessment'
-          icon={<Organization20Regular fontSize={iconSize} />}
+          icon={<ClipboardCheckmark24Regular fontSize={iconSize} />}
           tabs={['My Assessments', 'Team Performances']}
           selectedTab={selectedTab}
         />
         <MyPerformanceAgreement
           title='My Performance Agreement'
-          icon={<Organization20Regular fontSize={iconSize} />}
+          icon={<Handshake24Regular fontSize={iconSize} />}
           tabs={['My Quarterly Targets']}
           selectedTab={selectedTab}
         />
         <OrganizationPerformance
           title='Organization Performance'
-          icon={<Organization20Regular fontSize={iconSize} />}
+          icon={<DataTrending24Regular fontSize={iconSize} />}
           tabs={['Performance Evaluations', 'Organization Performance']}
           selectedTab={selectedTab}
         />
         <AnnualCorporateScorecard
           title="Annual Corporate Scorecard"
-          icon={<PeopleTeamRegular fontSize={iconSize} />}
+          icon={<Globe24Regular  fontSize={iconSize} />}
           tabs={['Quarterly Targets', 'Annual Targets']}
           selectedTab={selectedTab}
         />
         <Teams
           title='Teams'
-          icon={<BoardSplitRegular fontSize={iconSize} />}
+          icon={<PeopleTeam24Regular fontSize={iconSize} />}
           tabs={['Teams']}
           selectedTab={selectedTab}
         />
