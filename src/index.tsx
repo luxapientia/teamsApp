a67a934f405ec2fbce2be6d2685b5e704768a58b
Javid Jarafrov <javid.jafarov.ag@gmail.com>
2025-04-07 23:54:41 +0300

    revert speed upgrade

--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-// Use lazy initial render
-const renderApp = () => {
-  const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-  );
-  
-  root.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  );
-};
-
-// Check if we should defer hydration for better FCP
-if (window.requestIdleCallback) {
-  window.requestIdleCallback(renderApp);
-} else {
-  // Fallback for browsers that don't support requestIdleCallback
-  setTimeout(renderApp, 1);
-} 
+  </React.StrictMode>
+); 
