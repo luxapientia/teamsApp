5edcce89c129f4bdc10a62104f7e374c0eb9e0b1
Truong Pham <skyline.dreamer000@gmail.com>
2025-05-04 18:47:08 +0200

    feat: add strategy map

---
+++ src/pages/dashboard/components/strategyMap.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+const StrategyMap: React.FC = () => {
+  return (
+    <div>
+      <Typography variant="h6" sx={{ mb: 2 }}>
+        Strategy Map View
+      </Typography>
+    </div>
+  );
+};
+
+export default StrategyMap;
